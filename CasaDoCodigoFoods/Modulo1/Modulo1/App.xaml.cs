a690b6be9702f286aa45272e2d549741276141bb
Éderson Fernandes <edersonluis.rf@gmail.com>
2021-10-24 11:57:34 -0300

    3.1 CRIAÇÃO DA APLICAÇÃO

---
+++ CasaDoCodigoFoods/Modulo1/Modulo1/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Modulo1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //InitializeComponent();
+
+            var content = new ContentPage
+            {
+                Title = "Modulo1",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            //MainPage = new MainPage();
+            MainPage = content;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
